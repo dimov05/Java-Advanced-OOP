09ebc183160f8910399e21a5e3928d8598294bf8
dimov05 <dimov.1999@gmail.com>
2022-06-28 15:02:16 +0300

    New commit

---
+++ OOP/Java-OOP/src/Inheritance/RandomArrayList/Main.java
@@ -0,0 +1,9 @@
+package Inheritance.RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RandomArrayList<Integer> list = new RandomArrayList<>();
+
+    }
+}
