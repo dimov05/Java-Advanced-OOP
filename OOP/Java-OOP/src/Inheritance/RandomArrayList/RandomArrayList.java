d375038da41561f27f196c1baa3e30c35f2b746c
dimov05 <dimov.1999@gmail.com>
2022-06-29 16:54:01 +0300

    New commit

---
+++ OOP/Java-OOP/src/Inheritance/RandomArrayList/RandomArrayList.java
@@ -0,0 +1,18 @@
+package Inheritance.RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+    private Random random;
+
+    public RandomArrayList(){
+        this.random = new Random();
+    }
+
+    public T getRandomElement(){
+        int index = this.random.nextInt(this.size());
+        return super.remove(index);
+    }
+
+}
