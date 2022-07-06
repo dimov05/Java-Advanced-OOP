3059795d48fd1e2b97fd2227d8870bcf12f55d0a
dimov05 <ddimov.1999@gmail.com>
2022-07-06 09:05:24 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/Restaurant/restaurant/Food.java
@@ -0,0 +1,16 @@
+package InheritanceExercise.Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class Food extends Product{
+    private double grams;
+
+    public double getGrams() {
+        return grams;
+    }
+
+    public Food(String name, BigDecimal price, double grams){
+        super(name, price);
+        this.grams = grams;
+    }
+}
