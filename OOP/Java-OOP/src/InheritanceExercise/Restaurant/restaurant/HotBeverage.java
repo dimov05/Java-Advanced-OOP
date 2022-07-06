c1097e3acb2bd9b5e24420e8c557b7991fbd3c97
dimov05 <ddimov.1999@gmail.com>
2022-07-06 09:05:04 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/Restaurant/restaurant/HotBeverage.java
@@ -0,0 +1,9 @@
+package InheritanceExercise.Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage{
+    public HotBeverage(String name, BigDecimal price, double milliliters){
+        super(name, price, milliliters);
+    }
+}
