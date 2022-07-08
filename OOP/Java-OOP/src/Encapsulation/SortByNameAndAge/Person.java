76d0b194e0c9a51d1ab745773ce06492f8f6ca16
dimov05 <ddimov.1999@gmail.com>
2022-07-08 16:00:42 +0300

    New commit

---
+++ OOP/Java-OOP/src/Encapsulation/SortByNameAndAge/Person.java
@@ -0,0 +1,33 @@
+package Encapsulation.SortByNameAndAge;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %d years old.",
+                this.firstName, this.lastName, this.age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+}
