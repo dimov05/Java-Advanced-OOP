78bf0e701461d25296f898fd57c54028c14a1f68
dimov05 <ddimov.1999@gmail.com>
2022-08-03 15:36:25 +0300

    New commit

---
+++ OOP/Java-OOP/src/Cat.java
@@ -0,0 +1,12 @@
+public class Cat  extends Animal{
+
+    protected Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("%s%nMEEOW",
+                super.explainSelf());
+    }
+}
