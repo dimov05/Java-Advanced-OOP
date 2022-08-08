4f96faec674e86f85b955834cc8171a949e08a92
dimov05 <ddimov.1999@gmail.com>
2022-08-08 08:49:11 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/Animals/animals/Frog.java
@@ -0,0 +1,11 @@
+package InheritanceExercise.Animals.animals;
+
+public class Frog extends Animal{
+    public Frog(String name,int age,Gender gender){
+        super(name, age, gender);
+    }
+    @Override
+    public String produceSound(){
+        return "Ribbit";
+    }
+}
