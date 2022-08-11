c3bcdb7885b48584f811f08f64bc111dd2773387
dimov05 <ddimov.1999@gmail.com>
2022-08-11 12:08:25 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/NeedForSpeed/Vehicle.java
@@ -0,0 +1,56 @@
+package InheritanceExercise.NeedForSpeed;
+
+public class Vehicle {
+    private static final double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    void drive(double kilometers) {
+        if (enoughFuel(kilometers)) {
+            this.setFuel(this.getFuel()-(this.getFuelConsumption()*kilometers));
+        }
+    }
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    private boolean enoughFuel(double kilometers) {
+        if (this.getFuelConsumption() * kilometers >= this.getFuel()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+}
