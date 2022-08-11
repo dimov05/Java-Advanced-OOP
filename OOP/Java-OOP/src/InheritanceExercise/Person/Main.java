0b65bb9501a64592b651dc3eeb19cd24ad3400a9
dimov05 <ddimov.1999@gmail.com>
2022-08-11 12:08:39 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/Person/Main.java
@@ -0,0 +1,20 @@
+package InheritanceExercise.Person;
+
+import InheritanceExercise.Person.person.Child;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Child child = new Child(name,age);
+
+        System.out.println(child.getName());
+        System.out.println(child.getAge());
+
+    }
+}
