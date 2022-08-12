39f4570b63a1ad957adbeaa5dc7c552b41866c97
dimov05 <ddimov.1999@gmail.com>
2022-08-12 15:34:24 +0300

    New Commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/PlayersAndMonsters/hero/BladeKnight.java
@@ -0,0 +1,7 @@
+package InheritanceExercise.PlayersAndMonsters.hero;
+
+public class BladeKnight extends DarkKnight{
+    public BladeKnight(String username, int level){
+        super(username, level);
+    }
+}
