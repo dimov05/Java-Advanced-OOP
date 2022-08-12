82bbaa03b65b7a90cb0be39158c15f76c214d072
dimov05 <ddimov.1999@gmail.com>
2022-08-12 15:33:17 +0300

    New Commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/Person/person/Person.java
@@ -0,0 +1,19 @@
+package InheritanceExercise.Person.person;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
