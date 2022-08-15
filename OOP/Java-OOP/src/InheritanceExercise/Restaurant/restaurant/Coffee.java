a8f34b8165b9b4a90c66d269311087f5da32eb86
dimov05 <ddimov.1999@gmail.com>
2022-08-15 09:52:48 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/Restaurant/restaurant/Coffee.java
@@ -0,0 +1,18 @@
+package InheritanceExercise.Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class Coffee extends HotBeverage{
+    private static final double COFFEE_MILLILITERS = 50;
+    private static final BigDecimal COFFEE_PRICE = BigDecimal.valueOf(3.50);
+    private double caffeine;
+
+    public double getCaffeine() {
+        return caffeine;
+    }
+
+    public Coffee(String name, double caffeine){
+        super(name, COFFEE_PRICE, COFFEE_MILLILITERS);
+        this.caffeine = caffeine;
+    }
+}
