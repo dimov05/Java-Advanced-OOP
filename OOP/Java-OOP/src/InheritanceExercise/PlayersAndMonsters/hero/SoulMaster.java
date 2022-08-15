1ce2e9ef5163722452c4a5713d831dbc6b0f1b80
dimov05 <ddimov.1999@gmail.com>
2022-08-15 09:49:32 +0300

    New commit

---
+++ OOP/Java-OOP/src/InheritanceExercise/PlayersAndMonsters/hero/SoulMaster.java
@@ -0,0 +1,7 @@
+package InheritanceExercise.PlayersAndMonsters.hero;
+
+public class SoulMaster extends DarkWizard{
+    public SoulMaster(String username, int level){
+        super(username, level);
+    }
+}
