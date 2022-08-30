50c004d2e1bc96d0312f840f4e8b63371adeac96
dimov05 <ddimov.1999@gmail.com>
2022-08-30 12:59:21 +0300

    Interface and Abstraction exercise

---
+++ OOP/Java-OOP/src/InterfaceAndAbstractionExercise/MilitaryElite/Main.java
@@ -0,0 +1,11 @@
+package InterfaceAndAbstractionExercise.MilitaryElite;
+
+import InterfaceAndAbstractionExercise.MilitaryElite.Engine.Engine;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Engine.run();
+    }
+}
