2bda8e8725361b5aa0babe406d6ed1fbff4d290f
dimov05 <ddimov.1999@gmail.com>
2022-09-01 11:25:51 +0300

    Unit testing lab

---
+++ OOP-Maven/src/main/java/rpg_lab/Hero.java
@@ -0,0 +1,42 @@
+package rpg_lab;
+
+import java.util.Random;
+
+public class Hero {
+
+    private String name;
+    private int experience;
+    private Weapon weapon;
+
+    public Hero(String name, Weapon weapon) {
+        this.name = name;
+        this.experience = 0;
+        this.weapon = weapon;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getExperience() {
+        return this.experience;
+    }
+
+    public Weapon getWeapon() {
+        return this.weapon;
+    }
+
+    public Weapon attack(Target target, Random random) {
+        this.weapon.attack(target);
+
+        if (target.isDead()) {
+            this.experience += target.giveExperience();
+            return target.dropWeapon(random);
+        }
+
+        return null;
+    }
+
+    public void attack(Target target) {
+    }
+}
