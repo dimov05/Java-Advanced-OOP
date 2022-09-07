459239b0c1d3b8ecf314034223230f4c3299e904
dimov05 <ddimov.1999@gmail.com>
2022-09-07 08:59:32 +0300

    Test driven developement - exercise

---
+++ OOP-Maven/src/main/java/TestDrivenDevelopmentExercise/TransactionImpl.java
@@ -0,0 +1,52 @@
+package TestDrivenDevelopmentExercise;
+
+public class TransactionImpl implements Comparable<TransactionImpl>, Transaction {
+
+    private int id;
+    private TransactionStatus status;
+    private String from;
+    private String to;
+    private double amount;
+
+    public TransactionImpl(int id, TransactionStatus status, String from, String to, double amount) {
+        this.id = id;
+        this.status = status;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+
+    public int compareTo(TransactionImpl o) {
+        return 0;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public TransactionStatus getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public void setStatus(TransactionStatus newStatus) {
+        this.status = newStatus;
+    }
+
+    @Override
+    public double getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public String getFrom() {
+        return this.from;
+    }
+
+    @Override
+    public String getTo() {
+        return this.to;
+    }
+}
