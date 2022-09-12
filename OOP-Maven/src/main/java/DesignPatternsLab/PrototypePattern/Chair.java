1ca40d385bc1be34e6da0c89079d0545c6e13799
dimov05 <ddimov.1999@gmail.com>
2022-09-12 08:19:35 +0300

    Design patterns lab - Prototype pattern

---
+++ OOP-Maven/src/main/java/DesignPatternsLab/PrototypePattern/Chair.java
@@ -0,0 +1,24 @@
+package DesignPatternsLab.PrototypePattern;
+
+public class Chair implements Prototype {
+    private int height;
+    private int width;
+    private String color;
+
+    public Chair(int height, int width, String color) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+
+    private Chair(Chair chair) {
+        this.height = chair.height;
+        this.width = chair.width;
+        this.color = chair.color;
+    }
+
+    @Override
+    public Chair clone() {
+        return new Chair(this);
+    }
+}
