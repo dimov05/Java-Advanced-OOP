7339847e8485906cd8c34cabc8e4b76a0314d4bd
dimov05 <ddimov.1999@gmail.com>
2022-10-03 09:12:26 +0300

    Inheritance

---
+++ OOP/Java-OOP/src/Inheritance/MultipleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package Inheritance.MultipleInheritance;
+
+public class Animal{
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ OOP/Java-OOP/src/Inheritance/MultipleInheritance/Puppy.java
@@ -0,0 +1,7 @@
+package Inheritance.MultipleInheritance;
+
+public class Puppy extends Dog {
+    public void weep() {
+        System.out.println("weeping...");
+    }
+}
