8eeffac50c2a983dd7c01ddfe4a581c7dbe7f2fe
dimov05 <ddimov.1999@gmail.com>
2022-10-05 11:22:16 +0300

    Reflection and Annotation

---
+++ OOP/Java-OOP/src/ReflectionAndAnnotationExercise/BarracksWars/barracksWars/models/units/Gunner.java
@@ -0,0 +1,11 @@
+package ReflectionAndAnnotationExercise.BarracksWars.barracksWars.models.units;
+
+public class Gunner extends AbstractUnit{
+
+    private static final int GUNNER_HEALTH = 20;
+    private static final int GUNNER_DAMAGE = 20;
+
+    protected Gunner() {
+        super(GUNNER_HEALTH, GUNNER_DAMAGE);
+    }
+}
