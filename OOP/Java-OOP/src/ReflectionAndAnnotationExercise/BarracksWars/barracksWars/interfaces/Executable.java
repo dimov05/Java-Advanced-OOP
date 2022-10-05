69926fcc5376f8ad5b32df95b8fdc1b61f86afc8
dimov05 <ddimov.1999@gmail.com>
2022-10-05 11:20:51 +0300

    Reflection and Annotation

---
+++ OOP/Java-OOP/src/ReflectionAndAnnotationExercise/BarracksWars/barracksWars/interfaces/Executable.java
@@ -0,0 +1,11 @@
+package ReflectionAndAnnotationExercise.BarracksWars.barracksWars.interfaces;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface Executable {
+
+	String execute() throws ExecutionControl.NotImplementedException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+
+}
---
+++ OOP/Java-OOP/src/ReflectionAndAnnotationExercise/BarracksWars/barracksWars/interfaces/Runnable.java
@@ -0,0 +1,5 @@
+package ReflectionAndAnnotationExercise.BarracksWars.barracksWars.interfaces;
+
+public interface Runnable {
+	void run();
+}
